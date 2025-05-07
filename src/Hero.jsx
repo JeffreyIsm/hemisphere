7a6060f7b0c4eacc9bdeee32ecffd6f769ee6c02
JeffreyIsm <globalmonkey100@gmail.com>
2025-05-07 21:21:28 +0800

    changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,6 +1,6 @@
 function Hero(){
     return(
-        <div className='bg-hemi h-120 flex-col place-content-center'>
+        <div className='bg-hemi flex-col place-content-center'>
             <h1 className='text-5xl flex justify-center animate-fade-in-scale'>hemisphere</h1>
             <h2 className='text-xl flex justify-center mt-9 animate-fade-in-scale-delayed'>one mind at a time</h2>
         </div>
